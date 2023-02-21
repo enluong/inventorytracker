cfabae2dec47958a2cbc95357f8b98c9d38b87f1
Erin <123014461+enluong@users.noreply.github.com>
2023-02-20 20:47:01 -0800

    Update InventorySearchView.swift

--- inventorytracker/InventorySearchView.swift
+++ inventorytracker/InventorySearchView.swift
@@ -1,17 +1,27 @@
 //
-//  ProfileView.swift
+//  InventorySearchView.swift
 //  inventorytracker
 //
-//  Created by Erin on 2/15/23.
+//  Created by Alfian Losari on 29/05/22.
 //
+//  Modified by Team SEA 2023
+//
+//  ProfileSearchView -- done checking 2
 
 import SwiftUI
 
 struct InventorySearchView: View {
+    
+    // placeholder for 'items' array but for a a singular 'item'
     @EnvironmentObject var item: InventoryListViewModel
+    
+    // itemLookup calls for file that fetches item from keyword search
     @StateObject var itemLookup = InventoryLookupViewModel()
+    
+    // keyword is just keyword sequence
     @State var keyword = ""
     
+    // this is for actual search function on simulator/physical
     var body: some View {
         let keywordBinding = Binding<String>(
             get: {
@@ -19,23 +29,25 @@ struct InventorySearchView: View {
             },
             set: {
                 keyword = $0
-                itemLookup.fetchItem(from: keyword)
+                itemLookup.fetchItem(with: keyword)
             }
         )
         VStack {
             SearchBarView(keyword: keywordBinding)
             ScrollView {
-                ForEach(itemLookup.queriedItem, id: \.name) { user in
-//                    ProfileBarView(item: InventoryItem)
+                ForEach(itemLookup.queryResultItems, id: \.id) { item in
+                    InventoryBarView(item: item)
                 }
             }
         }
         .navigationBarHidden(true)
         .navigationBarTitleDisplayMode(.inline)
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        // stretches search bar
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
+        
+// design of search bar
 struct SearchBarView: View {
     @Binding var keyword: String
     
@@ -55,22 +67,28 @@ struct SearchBarView: View {
     }
 }
 
-//struct ProfileBarView: View {
-//    var item: InventoryItem
-//
-//    var body: some View {
-//        ZStack {
-//            Rectangle()
-//            .foregroundColor(Color.gray.opacity(0.2))
-//            HStack {
-//                Text("\(item.name)")
-//                Spacer()
-//                Text("\(item.quantity) \(item.type)")
-//            }
-//            .padding(.horizontal, 10)
-//        }
-//        .frame(maxWidth: .infinity, minHeight: 100)
-//        .cornerRadius(13)
-//        .padding()
-//    }
-//}
+// design for actual search results
+struct InventoryBarView: View {
+    var item: InventoryItem
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.gray.opacity(0.2))
+            HStack {
+                Text("\(item.location)")
+                Spacer()
+                Text("\(item.name)")
+                Spacer()
+                Text("\(item.quantity)")
+                Spacer()
+                Text("\(item.type)")
+            }
+            .padding(.horizontal, 10)
+        }
+        .frame(maxWidth: .infinity, minHeight: 100)
+        .cornerRadius(13)
+        .padding()
+    }
+}
+    
