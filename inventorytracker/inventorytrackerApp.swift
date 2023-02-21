7f65afc2f3f77d47fb4c37fd62e7aa6e39eaf99e
Erin <123014461+enluong@users.noreply.github.com>
2023-02-20 20:47:54 -0800

    Update inventorytrackerApp.swift

--- inventorytracker/inventorytrackerApp.swift
+++ inventorytracker/inventorytrackerApp.swift
@@ -5,6 +5,8 @@
 //  Created by Alfian Losari on 29/05/22.
 //
 //  Modified by Team SEA 2023
+//
+//  UserAuthenticationApp - done checking 2
 
 import FirebaseCore
 import FirebaseFirestore
@@ -12,24 +14,28 @@ import Firebase
 import SwiftUI
 import UIKit
 
-// connects to cloud firestore
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        let settings = Firestore.firestore().settings
+        Firestore.firestore().settings = settings
         return true
     }
+    
 }
 
 @main
 struct inventorytrackerApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
     var body: some Scene {
         WindowGroup {
 //            NavigationView {
-                ContentView()
+            let item = InventoryListViewModel()
+            ContentView()
+            .environmentObject(item)
 //            }
         }
     }
