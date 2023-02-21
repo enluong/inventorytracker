01876a14d85572c248faa0857d11b49c9c42e148
Erin <123014461+enluong@users.noreply.github.com>
2023-02-20 20:46:09 -0800

    Update InventoryListViewModel.swift

--- inventorytracker/InventoryListViewModel.swift
+++ inventorytracker/InventoryListViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Alfian Losari on 29/05/22.
 //
 //  Modified by Team SEA 2023
+//
+// UserViewModel
 
 import FirebaseFirestore
 import SwiftUI
@@ -14,31 +16,45 @@ class InventoryListViewModel: ObservableObject {
     
     private let db = Firestore.firestore().collection("inventory")
     
+    // placeholder array for items that are being searched
+    @Published var queryResultItems: [InventoryItem] = []
+    
     @Published var selectedSortType = SortType.createdAt
     @Published var isDescending = true
+    
+    // placeholder variables for editing
     @Published var editedItemName = ""
     @Published var editedItemLocation = ""
     @Published var editedItemType = ""
     @Published var editedItemCabinet = ""
     
+    // idk
+    var id: String?
+    
+    // for sort by function
     var predicates: [QueryPredicate] { [.order(by: selectedSortType.rawValue, descending: isDescending)] }
     
+    // adding item in array InventoryItem
     func addItem() {
         let item = InventoryItem(location: "Unknown", name: "New Item", quantity: 1, type:"Unknown", cabinet:"Unknown")
         _ = try? db.addDocument(from: item)
-        // *
+        
+        // supposed to add the new keywords according to new item
         db.document().updateData(["keywordsForLookup": item.keywordsForLookup])
     }
     
+    // update item in array InventoryItem
     func updateItem(_ item: InventoryItem, data: [String: Any]) {
         guard let id = item.id else { return }
         var _data = data
         _data["updatedAt"] = FieldValue.serverTimestamp()
         db.document(id).updateData(_data)
-        // *
+        
+        // supposed to update keywords according to update item
         db.document().updateData(["keywordsForLookup": item.keywordsForLookup])
     }
     
+    // deletes item in array InventoryItem
     func onDelete(items: [InventoryItem], indexset: IndexSet) {
         for index in indexset {
             guard let id = items[index].id else { continue }
@@ -86,6 +102,10 @@ class InventoryListViewModel: ObservableObject {
         }
     }
 }
+        
+    
+
+
         
     
 
