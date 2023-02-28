33fb2fe3aaf28b3863a704ee0ede66a8c5840e66
Erin <123014461+enluong@users.noreply.github.com>
2023-02-27 18:13:50 -0800

    Update InventorySearchView.swift

--- inventorytracker/InventorySearchView.swift
+++ inventorytracker/InventorySearchView.swift
@@ -52,21 +52,97 @@ struct InventorySearchView: View {
     
     private var items: [InventoryItem]
     
-    // listvm links to InventoryListViewModel class
-    @StateObject private var listvm = InventoryListViewModel()
-    
     // holds the search query entered by the user
-    @State private var searchText = ""
+    @State private var searchText: String = ""
+    
+    // filtered items based on search query
+        private var filteredItems: [InventoryItem] {
+            if searchText.isEmpty {
+                return items
+            } else {
+                return items.filter { item in
+                    return item.name.localizedCaseInsensitiveContains(searchText)
+                }
+            }
+        }
     
     // what you see on iPad simulator
     var body: some View {
         VStack {
+            // search bar
             InventorySearchBar(searchText: $searchText)
+                .padding(.top, 8)
             
-            // error if 'items' cannot be displayed due to data error in Firestore db
-            if let error = $items.error {
-                Text(error.localizedDescription)
+            // the order of what is being displayed
+            List {
+                // items from db
+                listItemsSectionView
+            }
+            // possibly the white box around each displays
+            .listStyle(.insetGrouped)
+        }
+    }
+    
+    // how 'items' display information on iPad for each item detail
+    private var listItemsSectionView: some View {
+        Section {
+            // this is where 'items' turn into 'item' in order to display each item
+            ForEach(filteredItems) { item in
+                VStack {
+                    // hstack keeps text and textfield next to each other
+                    // ITEM LOCATION
+                    HStack {
+                        Text("Location:")
+                        TextField("Location", text: Binding<String>(
+                            get: { item.location },
+                            set: { updateItem(item, data: ["location": $0]) }
+                        ))
+                    }
+                    
+                    // ITEM CABINET
+                    HStack{
+                        Text("Cabinet:")
+                        TextField("Cabinet", text: Binding<String>(
+                            get: { item.cabinet },
+                            set: { updateItem(item, data: ["cabinet": $0]) }
+                        ))
+                    }
+                    
+                    // ITEM NAME
+                    HStack {
+                        Text("Item Name:")
+                        TextField("Name", text: Binding<String>(
+                            get: { item.name },
+                            set: { updateItem(item, data: ["name": $0]) }
+                        ))
+                    }
+                    
+                    // ITEM QUANTITY
+                    Stepper("Quantity: \(item.quantity)",
+                            value: Binding<Int>(
+                                get: { item.quantity },
+                                set: { updateItem(item, data: ["quantity": $0]) }),
+                            in: 0...1000)
+                    
+                    // ITEM TYPE
+                    HStack{
+                        Text("Quantity Type:")
+                        TextField("Type", text: Binding<String>(
+                            get: { item.type },
+                            set: { updateItem(item, data: ["type": $0]) }
+                        ))
+                    }
+                }
             }
         }
+    }
+    
+    // update item in array InventoryItem
+    func updateItem(_ item: InventoryItem, data: [String: Any]) {
+        let db = Firestore.firestore().collection("inventory")
+        guard let id = item.id else { return }
+        var _data = data
+        _data["updatedAt"] = FieldValue.serverTimestamp()
+        db.document(id).updateData(_data)
     }
 }
