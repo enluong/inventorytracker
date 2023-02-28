ba3fecabec63d44edd817a94a5b96d4292d365bb
Erin <123014461+enluong@users.noreply.github.com>
2023-02-27 17:17:24 -0800

    Update InventoryItem.swift

--- inventorytracker/InventoryItem.swift
+++ inventorytracker/InventoryItem.swift
@@ -2,18 +2,16 @@
 //  InventoryItem.swift
 //  inventorytracker
 //
-//  Created by Alfian Losari on 29/05/22.
+//  Referencing Alfian Losari
 //
-//  Modified by Team SEA 2023
+//  by Team SEA 2023
 //
-//  User - done checking
 
 import Foundation
 import FirebaseFirestoreSwift
 
 struct InventoryItem: Identifiable, Codable {
     
-    // reference to uuid from search bar YT video
     // variable that holds documentID
     @DocumentID var id: String?
     
@@ -26,23 +24,4 @@ struct InventoryItem: Identifiable, Codable {
     var quantity: Int
     var type: String
     var cabinet: String
-    
-    // for search bar: generates keywords for each InventoryItem detail
-    var keywordsForLookup: [String] {
-        [self.name.generateStringSequence(), self.location.generateStringSequence(), self.type.generateStringSequence(), self.cabinet.generateStringSequence()].flatMap { $0 }
-    }
-    
 }
-
-// for search bar: generates string seqeuences ex: 'Item' -> 'I', 'It', 'Ite', 'Item'
-extension String {
-    func generateStringSequence() -> [String] {
-        guard self.count > 0 else { return [] }
-        var sequences: [String] = []
-        for i in 1...self.count {
-            sequences.append(String(self.prefix(i)))
-        }
-        return sequences
-    }
-}
-
